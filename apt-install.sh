7620bf8000391c37557567a6418f3a7bd4ea8b28
Karl Chen <kchen@sonicwall.com>
2019-07-27 04:47:30 -0700

    remove -y option

--- apt-install.sh
+++ apt-install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #
 #
+#APT_OPT="-y"
+#
 while [ "$1" != "" ] ; 
 do
    echo -e "\n INFO: installing $1\n"
-   sudo apt install $1 -y
+   sudo apt install $1 ${APT_OPT}
    if [ $? -ne 0 ] ; then 
         echo "\nERROR: install $1 failed!\n"
         exit 1
