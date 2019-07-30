805917746d62d6351de8f626f3361c820115beb6
Karl Chen <kchen@sonicwall.com>
2019-07-29 19:56:49 -0700

    add wimtools, package support for bootiso script

--- apt-install.sh
+++ apt-install.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 #
-#
 #APT_OPT="-y"
 #
+if [ "${CNT_FILE}" = "" ] ; then 
+    CNT_FILE="./.cnt-installed"
+fi
+N_INSTALLED=`cat ${CNT_FILE}`
 while [ "$1" != "" ] ; 
 do
-   echo -e "\n INFO: installing $1\n"
+   echo -e "\n INFO: LOOP:#${N_INSTALLED} installing $1\n"
    sudo apt install $1 ${APT_OPT}
    if [ $? -ne 0 ] ; then 
-        echo "\nERROR: install $1 failed!\n"
+        echo "\nERROR: LOOP:#{N_INSTALLED} installing $1 failed!\n"
+        echo "${N_INSTALLED}" >${CNT_FILE} 
         exit 1
    fi
-   shift 1 
+   shift 1
+   N_INSTALLED=$((N_INSTALLED+1)) 
 done
+echo "${N_INSTALLED}" >${CNT_FILE}
 
