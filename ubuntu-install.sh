7b937714dacdb8de3d7a3b6d66ebd43652ba555d
Karl Chen <kchen@sonicwall.com>
2019-09-12 02:58:28 -0700

    update add htop

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -17,8 +17,9 @@ echo "0" >${CNT_FILE}
 ./apt-install.sh libwine libx86-1 pm-utils powerwake python3-pip python3-setuptools schroot schroot-common uck ubuntu-gnome-desktop vbetool vnc4server xvnc4viewer xclip rpm nano | tee install.log6
 # for 'bootiso' script, install wimtools
 # bootiso write iso to usb stick as bootable disk 
-./apt-install.sh wimtools xinetd tftpd tftp gitk gparted dos2unix pv btrfs-progs stressapptest | tee install.log7
+./apt-install.sh wimtools xinetd tftpd tftp gitk gparted dos2unix pv btrfs-progs stressapptest htop snapd | tee install.log7
 ./apt-install.sh bmon make libconfuse-dev libnl-3-dev libnl-route-3-dev libncurses-dev pkg-config dh-autoreconf | tee install.log8
+sudo snap install htop
 #
 N_INSTALLED=`cat ${CNT_FILE}`
 echo -e "\n INFO: ${N_INSTALLED} packages checked or installed\n" 
