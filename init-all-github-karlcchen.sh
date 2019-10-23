4be71e94e23486c2ab601d6f1e7358441415c4fd
Karl Chen <kchen@sonicwall.com>
2019-10-22 19:53:19 -0700

    update

--- init-all-github-karlcchen.sh
+++ init-all-github-karlcchen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
 cd
-~/sh-ubuntu-install/git-clone-karlcchen.sh sh-system-adm sh-ubuntu-install sh-glp2 sh-uboot sh-soniccorex sh-bitbake sh-lpower etc bin kchen test-uboot-cmd
+./git-clone-karlcchen.sh sh-system-adm sh-ubuntu-install sh-glp2 sh-uboot sh-soniccorex sh-bitbake sh-lpower etc bin kchen test-uboot-cmd
