293376403440dab7b01536e75d49aa9c416aada5
Karl Chen <kchen@sonicwall.com>
2019-10-22 19:50:49 -0700

    fix error

--- init-all-github-karlcchen.sh
+++ init-all-github-karlcchen.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #
 cd
-~/sh-ubuntu-install/git-one-karlcchen.sh sh-system-adm sh-ubuntu-install sh-glp2 sh-uboot sh-soniccorex sh-bitbake sh-lpower etc bin kchen test-uboot-cmd
-
+~/sh-ubuntu-install/git-clone-karlcchen.sh sh-system-adm sh-ubuntu-install sh-glp2 sh-uboot sh-soniccorex sh-bitbake sh-lpower etc bin kchen test-uboot-cmd
