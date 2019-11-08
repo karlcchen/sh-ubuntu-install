ac0b98df704834d81337ef95abbb135635a24402
Karl Chen <kchen@sonicwall.com>
2019-11-08 18:12:44 +0800

    update

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -19,13 +19,15 @@ echo "0" >${CNT_FILE}
 # bootiso write iso to usb stick as bootable disk 
 ./apt-install.sh wimtools xinetd tftpd tftp gitk gparted dos2unix pv btrfs-progs stressapptest htop snapd | tee install.log7
 ./apt-install.sh bmon make libconfuse-dev libnl-3-dev libnl-route-3-dev libncurses-dev pkg-config dh-autoreconf | tee install.log8
-sudo snap install htop u-boot-tools
 
 # for compiling uboot image 
+# for trusted uboot images: libmbedtls-dev libconfig-dev 
+./apt-install.sh install snap htop u-boot-tools libmbedtls-dev libconfig-dev | tee install.log9
 pip3 install numpy
 pip3 install elftools
 pip3 install pyelftools
-pip  install  pycrypto
+pip  install pycrypto
+
 
 #
 N_INSTALLED=`cat ${CNT_FILE}`
