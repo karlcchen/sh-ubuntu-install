ea260afdf2bad7cfa88c4e5c35041b1067a737dc
Karl Chen <kchen@sonicwall.com>
2020-07-15 10:49:05 -0700

    add sync ~/gg

--- sync-all-github-karlcchen.sh
+++ sync-all-github-karlcchen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
 cd
-~/sh-system-adm/git-pull.sh sh-system-adm sh-ubuntu-install sh-glp2 sh-uboot sh-soniccorex sh-bitbake sh-lpower etc bin kchen test-uboot-cmd
+~/sh-system-adm/git-pull.sh sh-system-adm sh-ubuntu-install sh-glp2 sh-uboot sh-soniccorex sh-bitbake sh-lpower etc bin kchen test-uboot-cmd gg
 
