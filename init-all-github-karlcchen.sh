02bcc78e291970c286988acbc7e2144754201f4d
Karl Chen <kchen@sonicwall.com>
2020-11-12 14:27:31 -0800

    update init-all-github-karlcchen.sh

--- init-all-github-karlcchen.sh
+++ init-all-github-karlcchen.sh
@@ -2,16 +2,29 @@
 #
 # init-all-github-karlcchen.sh
 #
+# running from "sh-ubuntu-install.saved"
+#
 CUR_DIR=`pwd`
 cd
-
-${CUR_DIR}/sh-ubuntu-install.saved/git-clone-karlcchen.sh sh-ubuntu-install
+${CUR_DIR}/git-clone-karlcchen.sh sh-ubuntu-install
 if [ $? -ne 0 ] ; then 
     printf '\nERROR-1: %s failed!\n' "${CUR_DIR}/sh-ubuntu-install.saved/git-clone-karlcchen.sh sh-ubuntu-install"
     exit 1 
 fi 
 
+cd sh-ubuntu-install
+if [ $? -ne 0 ] ; then 
+    printf '\nERROR-2: cd %s failed!\n' "sh-ubuntu-install"
+    exit 2 
+fi 
+
+CUR_DIR=`pwd`
+cd
 ${CUR_DIR}/git-clone-karlcchen.sh sh-system-adm sh-glp2 sh-uboot sh-soniccorex sh-bitbake sh-lpower etc bin kchen test-uboot-cmd 
+if [ $? -ne 0 ] ; then 
+    printf '\nERROR-3: %s failed!\n' "${CUR_DIR}/git-clone-karlcchen.sh"
+    exit 3 
+fi 
 
 #
 # gg is not from github
@@ -19,18 +32,18 @@ ${CUR_DIR}/git-clone-karlcchen.sh sh-system-adm sh-glp2 sh-uboot sh-soniccorex s
 cd
 mkdir gg 
 if [ $? -ne 0 ] ; then 
-    printf '\nERROR-2: mkdir %s failed!\n' "gg"
-    exit 2 
+    printf '\nERROR-4: mkdir %s failed!\n' "gg"
+    exit 4 
 fi 
 cd gg
 if [ $? -ne 0 ] ; then 
-    printf '\nERROR-3: cd %s failed!\n' "gg"
-    exit 3 
+    printf '\nERROR-5: cd %s failed!\n' "gg"
+    exit 5 
 fi 
 
 git clone git@sonicgit.eng.sonicwall.com:kchen/gg.git
 if [ $? -ne 0 ] ; then 
-    printf '\nERROR-4: %s failed!\n' "git clone git@sonicgit.eng.sonicwall.com:kchen/gg.git"
-    exit 4 
+    printf '\nERROR-6: %s failed!\n' "git clone git@sonicgit.eng.sonicwall.com:kchen/gg.git"
+    exit 6 
 fi 
 
