0d2a6a1f3c9724a5b6859aec74b5f89f36a7b5ff
Karl Chen <kchen@sonicwall.com>
2020-11-12 14:40:59 -0800

    update rm-all-git-dirs.sh

--- rm-all-git-dirs.sh
+++ rm-all-git-dirs.sh
@@ -7,12 +7,37 @@ CUR_DIR=`pwd`
 cd
 mv sh-ubuntu-install sh-ubuntu-install.saved
 if [ $? -ne 0 ] ; then 
-    printf '\nERROR-1: %s failed !\n' "mv sh-ubuntu-install sh-ubuntu-install.saved"
+    printf '\nERROR-1: %s failed!\n' "mv sh-ubuntu-install sh-ubuntu-install.saved"
     exit 1
 fi 
-${CUR_DIR}.saved/rm-git-dirs.sh sh-system-adm sh-glp2 sh-uboot sh-soniccorex sh-bitbake sh-lpower etc bin kchen test-uboot-cmd gg
+
+cd sh-ubuntu-install.saved
 if [ $? -ne 0 ] ; then 
-    printf '\nERROR-2: %s failed !\n' "${CUR_DIR}/rm-git-dirs.sh"
+    printf '\nERROR-2: cd %s failed !\n' "sh-ubuntu-install.saved"
     exit 2
 fi 
+
+./rm-git-dirs.sh sh-system-adm sh-glp2 sh-uboot sh-soniccorex sh-bitbake sh-lpower etc bin kchen test-uboot-cmd gg
+if [ $? -ne 0 ] ; then 
+    printf '\nERROR-3: %s failed !\n' "./rm-git-dirs.sh"
+    exit 3
+fi 
+
+cd 
+./sh-ubuntu-install.saved/git-clone-karlcchen.sh sh-ubuntu-install
+if [ $? -ne 0 ] ; then 
+    printf '\nERROR-4: %s failed!\n' "./sh-ubuntu-install.saved/git-clone-karlcchen.sh sh-ubuntu-install"
+    exit 4
+fi 
+
+cd
+cd sh-ubuntu-install
+if [ $? -ne 0 ] ; then 
+    printf '\nERROR-5: cd %s failed!\n' "sh-ubuntu-install"
+    exit 5
+fi 
+
+
+
+
 
