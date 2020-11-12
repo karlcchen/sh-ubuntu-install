ed847bc2847202ad9449c516da03df10377421cb
Karl Chen <kchen@sonicwall.com>
2020-11-12 14:16:16 -0800

    update rm-all-git-dirs.sh

--- rm-all-git-dirs.sh
+++ rm-all-git-dirs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
+# rm-all-dirs.sh
 #
+
 CUR_DIR=`pwd`
 cd
 mv sh-ubuntu-install sh-ubuntu-install.saved
@@ -8,7 +10,7 @@ if [ $? -ne 0 ] ; then
     printf '\nERROR-1: %s failed !\n' "mv sh-ubuntu-install sh-ubuntu-install.saved"
     exit 1
 fi 
-${CUR_DIR}/rm-git-dirs.sh sh-system-adm sh-glp2 sh-uboot sh-soniccorex sh-bitbake sh-lpower etc bin kchen test-uboot-cmd gg
+${CUR_DIR}.saved/rm-git-dirs.sh sh-system-adm sh-glp2 sh-uboot sh-soniccorex sh-bitbake sh-lpower etc bin kchen test-uboot-cmd gg
 if [ $? -ne 0 ] ; then 
     printf '\nERROR-2: %s failed !\n' "${CUR_DIR}/rm-git-dirs.sh"
     exit 2
