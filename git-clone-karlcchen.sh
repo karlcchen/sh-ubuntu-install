0912637a9d94c9e093dfa1758596bdf47fdf1732
Karl Chen <kchen@sonicwall.com>
2020-11-12 13:29:14 -0800

    update git-clone-karlcchen.sh init-all-github-karlcchen.sh

--- git-clone-karlcchen.sh
+++ git-clone-karlcchen.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 #KGHUB=https://github.com/karlcchen
 KGHUB=ssh://git@github.com/karlcchen
+LOOP=0
 while [ "$1" != "" ] ; 
 do 
+    LOOP=$((LOOP+1))    
+    if [ -d "$1" ] ; then 
+        printf '\nWARNING-1: loop=%d, path \"%s\" already exist!\n' ${LOOP} "$1"
+    else 
         GIT_URL="${KGHUB}/${1}"
         printf "\n git clone from: %s\n" "$GIT_URL"
-	git clone ${GIT_URL}
+        git clone ${GIT_URL}
 	if [ $? -ne 0 ] ; then 
-		printf "\n ERROR: git clone from %s/%s failed!\n" "${KGHUB}" "$1"
-		exit 1 		
+	    printf "\n ERROR: git clone from %s/%s failed!\n" "${KGHUB}" "$1"
+	    exit 1 		
 	fi
-       shift 1	
+   fi
+   shift 1	
 done
--- init-all-github-karlcchen.sh
+++ init-all-github-karlcchen.sh
@@ -2,4 +2,4 @@
 #
 CUR_DIR=`pwd`
 cd
-${CUR_DIR}/git-clone-karlcchen.sh sh-system-adm sh-glp2 sh-uboot sh-soniccorex sh-bitbake sh-lpower etc bin kchen test-uboot-cmd
+${CUR_DIR}/git-clone-karlcchen.sh sh-system-adm sh-glp2 sh-uboot sh-soniccorex sh-bitbake sh-lpower etc bin kchen test-uboot-cmd gg
