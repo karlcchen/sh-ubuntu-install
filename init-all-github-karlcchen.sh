f034e99c44e3cecee09da1bb22e0d1ea2e90d558
Karl Chen <kchen@sonicwall.com>
2020-11-12 14:11:48 -0800

    update init-all-github-karlcchen.sh rm-all-git-dirs.sh

--- init-all-github-karlcchen.sh
+++ init-all-github-karlcchen.sh
@@ -2,4 +2,17 @@
 #
 CUR_DIR=`pwd`
 cd
-${CUR_DIR}/git-clone-karlcchen.sh sh-system-adm sh-glp2 sh-uboot sh-soniccorex sh-bitbake sh-lpower etc bin kchen test-uboot-cmd gg
+
+${CUR_DIR}/sh-ubuntu-install.saved/git-clone-karlcchen.sh sh-ubuntu-install
+if [ $? -ne 0 ] ; then 
+    printf '\nERROR-1: %s failed!\n' "${CUR_DIR}/sh-ubuntu-install.saved/git-clone-karlcchen.sh sh-ubuntu-install"
+    exit 1 
+fi 
+
+${CUR_DIR}/git-clone-karlcchen.sh sh-system-adm sh-glp2 sh-uboot sh-soniccorex sh-bitbake sh-lpower etc bin kchen test-uboot-cmd 
+
+cd
+mkdir gg 
+cd gg
+git clone git@sonicgit.eng.sonicwall.com:kchen/gg.git
+
--- rm-all-git-dirs.sh
+++ rm-all-git-dirs.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 #
+#
+CUR_DIR=`pwd`
 cd
-./rm-git-dirs.sh sh-system-adm sh-ubuntu-install sh-glp2 sh-uboot sh-soniccorex sh-bitbake sh-lpower etc bin kchen test-uboot-cmd
+mv sh-ubuntu-install sh-ubuntu-install.saved
+if [ $? -ne 0 ] ; then 
+    printf '\nERROR-1: %s failed !\n' "mv sh-ubuntu-install sh-ubuntu-install.saved"
+    exit 1
+fi 
+${CUR_DIR}/rm-git-dirs.sh sh-system-adm sh-glp2 sh-uboot sh-soniccorex sh-bitbake sh-lpower etc bin kchen test-uboot-cmd gg
+if [ $? -ne 0 ] ; then 
+    printf '\nERROR-2: %s failed !\n' "${CUR_DIR}/rm-git-dirs.sh"
+    exit 2
+fi 
+
